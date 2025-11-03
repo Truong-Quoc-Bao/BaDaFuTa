66b723d1cee4d8aa30b90ebbc431dc24bfb683f9
Trương Quốc Bảo <166063783+Truong-Quoc-Bao@users.noreply.github.com>
2025-11-03 11:48:57 +0700

    Update order.route.ts Backend

--- BaDaFuTa_Food/backend-clean/src/modules/order/order.route.ts
+++ BaDaFuTa_Food/backend-clean/src/modules/order/order.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createCODOrder } from "./order.controller";
-
+import { createCODOrder, getOrder } from "./order.controller";
+// import { createCODOrder } from "./order.controller";
 const router = Router();
 router.post("/", createCODOrder);
+router.post("/getOrder", getOrder);
 export default router;
