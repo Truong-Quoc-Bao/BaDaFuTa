1034ac79ea1cbf32d9ad76437e27e00b31356676
Trương Quốc Bảo <166063783+Truong-Quoc-Bao@users.noreply.github.com>
2025-11-06 13:54:55 +0700

    Update Order Backend

--- BaDaFuTa_Food/backend-clean/src/modules/order/order.repository.ts
+++ BaDaFuTa_Food/backend-clean/src/modules/order/order.repository.ts
@@ -1,11 +1,5 @@
-import {
-  PrismaClient,
-  Prisma,
-  order_status,
-  PaymentStatus,
-  payment_method,
-} from "@prisma/client";
-import { OrderItemInput, GetOrderInput } from "./order.type";
+import { PrismaClient, Prisma, order_status, PaymentStatus, payment_method } from '@prisma/client';
+import { OrderItemInput, GetOrderInput } from './order.type';
 
 const prisma = new PrismaClient();
 export const postOrder = {
@@ -23,37 +17,30 @@ export const postOrder = {
       total_amount: bigint;
       status?: string;
       status_payment?: string;
-    }
+    },
   ) {
     const normalized = {
       ...data,
       status:
-        ((
-          data.status || "PENDING"
-        ).toUpperCase() as keyof typeof order_status) in order_status
-          ? ((data.status || "PENDING").toUpperCase() as order_status)
+        ((data.status || 'PENDING').toUpperCase() as keyof typeof order_status) in order_status
+          ? ((data.status || 'PENDING').toUpperCase() as order_status)
           : order_status.PENDING,
 
       status_payment:
-        ((
-          data.status_payment || "PENDING"
-        ).toUpperCase() as keyof typeof PaymentStatus) in PaymentStatus
-          ? ((data.status_payment || "PENDING").toUpperCase() as PaymentStatus)
+        ((data.status_payment || 'PENDING').toUpperCase() as keyof typeof PaymentStatus) in
+        PaymentStatus
+          ? ((data.status_payment || 'PENDING').toUpperCase() as PaymentStatus)
           : PaymentStatus.PENDING,
 
       // 🔹 Ép cứng payment_method = COD
-      payment_method: "COD" as payment_method,
+      payment_method: 'COD' as payment_method,
     };
 
     return tx.order.create({ data: normalized });
   },
 
   /**  🧾 Tạo danh sách order_item */
-  async createOrderItems(
-    tx: Prisma.TransactionClient,
-    order_id: string,
-    items: OrderItemInput[]
-  ) {
+  async createOrderItems(tx: Prisma.TransactionClient, order_id: string, items: OrderItemInput[]) {
     return tx.order_item.createMany({
       data: items.map((i) => ({
         order_id,
@@ -87,11 +74,8 @@ export const orderRepository = {
             },
           },
         },
-        payments: true,
-        merchant: true,
-        user: true,
       },
-      orderBy: { created_at: "desc" },
+      orderBy: { created_at: 'desc' },
     });
   },
 
