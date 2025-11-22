231df67fa97348b7f2a2a59261f068e77a92de6c
Thietam <tranthientam220404@gmail.com>
2025-11-22 12:42:20 +0700

    add backend voucher and  ignore frontend

--- BaDaFuTa_Food/frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ condition, redirectTo, children }) {
-  if (!condition) {
-    return <Navigate to={redirectTo} replace />;
-  }
-  return children;
-}
