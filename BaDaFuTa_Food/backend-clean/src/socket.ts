c924cc61c51ab505b04648ce5f01c960361985dd
Trương Quốc Bảo <166063783+Truong-Quoc-Bao@users.noreply.github.com>
2025-11-29 03:32:37 +0700

    add socket

--- BaDaFuTa_Food/backend-clean/src/socket.ts
+++ BaDaFuTa_Food/backend-clean/src/socket.ts
@@ -1,25 +1,31 @@
-import { Server } from "socket.io";
+import { Server as HttpServer } from "http";
+import { Server as IOServer, Socket } from "socket.io";
 
-export function setupSocket(server) {
-  const io = new Server(server, {
+export const initSocket = (server: HttpServer) => {
+  const io = new IOServer(server, {
     cors: {
-      origin: "*",
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://ba-da-fu-ta-partner.vercel.app',
+      ],
+      methods: ['GET', 'POST'],
+      credentials: true,
     },
   });
 
-  io.on("connection", (socket) => {
-    console.log("Merchant connected:", socket.id);
+  io.on("connection", (socket: Socket) => {
+    console.log("✅ Client connected:", socket.id);
 
-    socket.on("joinMerchant", (merchantId) => {
+    socket.on("joinMerchant", (merchantId: string) => {
+      console.log(`Merchant ${merchantId} joined`);
       socket.join(merchantId);
-      console.log(`Merchant ${merchantId} joined room ${merchantId}`);
+    });
+
+    socket.on("newOrder", (merchantId: string, orderData: any) => {
+      io.to(merchantId).emit("newOrder", orderData);
     });
   });
 
-  // Hàm push đơn mới
-  function pushNewOrderToMerchant(merchantId, orderData) {
-    io.to(merchantId).emit("newOrder", orderData);
-  }
-
-  return { io, pushNewOrderToMerchant };
-}
+  return io;
+};
